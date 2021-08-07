c1c98112221f7824a2bce6254fe61d77d5d7cb17
Kevin Tooley <kevin.tooley@snhu.edu>
2021-08-07 14:06:41 -0400

    - Add frame size to VideoPlayer to remove surrounding black bars

--- LearningApp/Views/ContentDetailView.swift
+++ LearningApp/Views/ContentDetailView.swift
@@ -12,42 +12,48 @@ struct ContentDetailView: View {
     
     @EnvironmentObject var model: ContentModel
     
+    // sample.mp4 is 1920 x 1080
+    let videoRatio: CGFloat = 1080 / 1920
+    
     var body: some View {
         
         let lesson = model.currentLesson
         let url = URL(string: Constants.videoHostUrl + (lesson?.video ?? ""))
         
-        VStack {
-        
-            // only show video if we get valid url
-            if url != nil {
-                VideoPlayer(player: AVPlayer(url: url!))
-            }
+        GeometryReader { geo in
+            VStack {
             
-            // Description
-            CodeTextView()
-            
-            // Next lesson button only if there is a next lesson
-            
-            if model.hasNextLesson() {
-                Button(action: {
-                    //advance the lesson
-                    model.nextLession()
-                }, label: {
-    //                Text("Next Lesson: \(model.currentModule!.content.lessons[model.currentLessonIndex + 1].title)")
-                    ButtonView(buttonColor: .green, textColor: .white, buttonText: "Next Lesson: \(model.currentModule!.content.lessons[model.currentLessonIndex + 1].title)")
-                })
+                // only show video if we get valid url
+                if url != nil {
+                    VideoPlayer(player: AVPlayer(url: url!))
+                        .frame(height: geo.size.width * videoRatio)
+                }
+                
+                // Description
+                CodeTextView()
+                
+                // Next lesson button only if there is a next lesson
+                
+                if model.hasNextLesson() {
+                    Button(action: {
+                        //advance the lesson
+                        model.nextLession()
+                    }, label: {
+        //                Text("Next Lesson: \(model.currentModule!.content.lessons[model.currentLessonIndex + 1].title)")
+                        ButtonView(buttonColor: .green, textColor: .white, buttonText: "Next Lesson: \(model.currentModule!.content.lessons[model.currentLessonIndex + 1].title)")
+                    })
+                }
+                else {
+                    Button(action: {
+                        model.currentContentSelected = nil
+                    }, label: {
+                        ButtonView(buttonColor: .green, textColor: .white, buttonText: "Complete Lesson")
+                    })
+                }
             }
-            else {
-                Button(action: {
-                    model.currentContentSelected = nil
-                }, label: {
-                    ButtonView(buttonColor: .green, textColor: .white, buttonText: "Complete Lesson")
-                })
-            }
+            .padding()
+            .navigationBarTitle(lesson?.title ?? "")
         }
-        .padding()
-        .navigationBarTitle(lesson?.title ?? "")
         
     }
 }
